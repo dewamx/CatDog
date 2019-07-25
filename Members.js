f8d3d876f34fbc5321fd1c8b64f0fcfeeb703db2
raxemmohamed <raxemmohamed@gmail.com>
2019-07-25 15:50:16 -0500

    side project/study

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'Juan',
+        email: '1@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Too',
+        email: '2@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 3,
+        name: 'Tree',
+        email: '3@gmail.com',
+        status: 'active'
+    }
+];
+
+module.exports = members;
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const logger = require('./middleware/logger');
+const members = require('./Members');
+
+const app = express();
+
+
+//innit middleware
+app.use(logger);
+
+//route gets all members
+app.get('/api/members', (req, res) => res.json(members));
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 7000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
